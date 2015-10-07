a1be86694a321a812ca747ea719c004b24a25a6a
eugena <eugena.mihailikova@gmail.com>
2015-10-07 19:02:16 +0500

    tests running

--- tests/test_models.py
+++ tests/test_models.py
@@ -10,7 +10,7 @@ Tests for `django-categorized-flatpages` models module.
 
 from django.test import TestCase
 
-# from cflatpages import models
+from cflatpages import models
 
 
 class TestCflatpages(TestCase):
@@ -18,8 +18,32 @@ class TestCflatpages(TestCase):
     def setUp(self):
         pass
 
-    def test_something(self):
-        pass
+    def test_category(self):
+        """
+        Category model
+        """
+        category = models.Category.objects.create(
+            title='foo',
+            slug='foo',
+            pk=1)
+
+        self.assertTrue(category.__str__(), 'foo')
+
+    def test_flatpage(self):
+        """
+        Flatpage model
+        """
+        category = models.Category.objects.create(
+            title='foo',
+            slug='foo',
+            pk=1)
+
+        page = models.CFlatPage.objects.create(
+            title='foo',
+            keywords='foo',
+            category=category, )
+
+        self.assertTrue(page.__str__(), 'foo')
 
     def tearDown(self):
         pass
